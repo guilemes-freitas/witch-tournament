1fd135cb5ca8b4fecf2ee4a376366bd0a284b90f
Guilherme Lemes de Freitas <guilemes300@gmail.com>
2021-05-07 09:46:58 -0300

    feature: wand details

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,23 +1,41 @@
 import { Component } from "react";
 import "./styles.css";
+import Wand from "../../assets/magic-wand.svg";
 
 class Card extends Component {
   render() {
     const { student } = this.props;
-    const { name, gender, house, patronus, alive, image } = student;
+    const { name, gender, house, patronus, alive, image, wand } = student;
+    const { wood, core, length } = wand;
     return (
       <div className="card">
-        <div className={house}>
+        <div className={`img-bg ${house}`}>
           <img className="studentImg" src={image} alt={name}></img>
           <h3 className="studentName">{name}</h3>
         </div>
         <div className="houseContainer">
-          <img
-            className="houseImg"
-            src={`../img/${house}.png`}
-            alt={house}
-          ></img>
-          <p>{house}</p>
+          <div className="house">
+            <img
+              className="houseImg"
+              src={`../img/${house}.png`}
+              alt={house}
+            ></img>
+            <p>{house}</p>
+          </div>
+          {wand && (
+            <div className="wand">
+              <details>
+                <summary>
+                  <img classname="wandIcon" src={Wand} alt="Wand" />
+                </summary>
+                <div className="wandDetails">
+                  {wood && <p className="wandDetail">Wood: {wood}</p>}
+                  {core && <p className="wandDetail">Core: {core}</p>}
+                  {length && <p className="wandDetail">Length: {length}cm</p>}
+                </div>
+              </details>
+            </div>
+          )}
         </div>
         <div className={`card-stats ${house}`}>
           <div className="stat">
